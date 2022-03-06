a8a113e9c52e1b931af6b66d2264957663e0857f
Asad Maraka <asadmaraka@Asads-MacBook-Air.local>
2022-03-06 17:39:37 +0200

    highscore added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let secretNumber= Math.trunc(Math.random()*20)+1;
-
 let score=20;
+let highScore=0;
 
 document.querySelector(".check").addEventListener("click",function(){
     const guess = Number(document.querySelector(".guess").value);
@@ -13,6 +13,10 @@ document.querySelector(".check").addEventListener("click",function(){
         document.querySelector(".message").textContent="🥳 Correct number";
         document.querySelector("body").style.backgroundColor="#60b347";
         document.querySelector(".number").style.width="30rem";
+        if(score>highScore){
+            highScore=score;
+            document.querySelector(".highscore").textContent=highScore;
+        }
     }else if(guess>secretNumber){
         if(score>1)
             {   document.querySelector(".message").textContent="📈 Too high!";
